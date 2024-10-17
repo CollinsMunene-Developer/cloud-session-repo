2cd34f3a26bb4c22d24260e583eadc5f3e43e454
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-10-17 17:31:48 +0300

    first commit

---
+++ src/Components/Post.jsx
@@ -0,0 +1,14 @@
+
+
+function Post() {
+
+    return(
+        <div>
+            <p>Collins</p>
+            <p>Am codinw and learning with udemy</p>
+        </div>
+    )
+
+}
+
+export default Post;
